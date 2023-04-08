e18752a567ea4d4aef73dde0354bd51999f252cb
valntyn <romanenko.valentyn1@gmail.com>
2023-04-08 20:00:03 +0300

    fresh start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+const todos = [
+  {id: 1, title: 'hi angular', completed: false},
+  {id: 2, title: 'i will learn you', completed: false},
+  {id: 3, title: '123', completed: true},
+  {id: 4, title: 'test test', completed: false},
+];
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +20,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-todo';
+  editing = false;
+  todos = todos;
+  todoForm = new FormGroup({
+    title: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(3),
+      ],
+    }),
+  });
+
+  get title() {
+    return this.todoForm.get('title') as FormControl;
+  }
+
+  handleTodoToggle(event: Event, todo: Todo) {
+    todo.completed = (event.target as HTMLInputElement).checked
+  }
+
+  get activeTodos() {
+    return this.todos.filter(todo => !todo.completed);
+  }
+
+  addTodo() {
+    if (this.title.invalid) {
+      return
+    }
+
+    const newTodo: Todo = {
+      id: +new Date(),
+      title: this.title.value as string,
+      completed: false,
+    };
+
+    this.todos.push(newTodo);
+    this.todoForm.reset();
+  }
 }
